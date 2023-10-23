0e207cc11ca0ce79a953d827eddf885bd43b9f77
Patrick Fuchs <p.fuchs@hm.edu>
2023-10-23 12:20:08 +0200

    Class Calendar

---
+++ mobile_app_organaizo/lib/Calendar/classes/date.dart
@@ -0,0 +1,19 @@
+class Calendar {
+  final int year;
+  final int month;
+  final String monthname;
+  final int week;
+  final int day;
+  final String timezone;
+  final int weekday;
+
+  Calendar(
+    this.year,
+    this.month,
+    this.week,
+    this.day,
+    this.timezone,
+    this.weekday,
+    this.monthname,
+  );
+}
