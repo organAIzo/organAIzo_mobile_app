3aeae77d1b996477b8287ea7c2a700a92a59ff7f
Sebi <segoe0101@outlook.com>
2023-10-28 00:23:44 +0200

    Navigation and Routing

--- mobile_app_organaizo/lib/main.dart
+++ mobile_app_organaizo/lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app_organaizo/pages/home.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:mobile_app_organaizo/pages/BasicCalendar.dart';
+import 'Navigation.dart';
 
 void main(){
-  runApp( const MyApp());
+  runApp(MaterialApp(
+    title: 'OrganAIzo',
+
+    initialRoute: '/Nav',
+
+    routes: {
+      '/': (context) => HomePage(),
+      '/kalender': (context) => TableBasicsExample(),
+      '/Nav': (context) => Navigation(),
+
+    }
+  ));
+
 }
+
 
 // Stateless = No dynamic data
 // Terminal r - hot reload 
@@ -15,16 +28,3 @@ void main(){
 // https://www.youtube.com/watch?v=1xipg02Wu8s&t=232s
 
 // Refactor with refactor
-
-
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: TableBasicsExample());
-      }}
--- mobile_app_organaizo/lib/pages/BasicCalendar.dart
+++ mobile_app_organaizo/lib/pages/BasicCalendar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:mobile_app_organaizo/Navigation.dart';
 
 import '../utils.dart';
 
