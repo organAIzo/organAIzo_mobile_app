80fc411008a27333bbdf4b408825d7b09380b6d7
Patrick Fuchs <p.fuchs@hm.edu>
2023-11-22 18:55:05 +0100

    --

--- mobile_app_organaizo/lib/main.dart
+++ mobile_app_organaizo/lib/main.dart
@@ -1,33 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_app_organaizo/pages/splash_screen.dart';
-//import 'package:mobile_app_organaizo/pages/home.dart';
-//import 'package:mobile_app_organaizo/pages/BasicCalendar.dart';
-//import 'package:mobile_app_organaizo/Navigation/Navigation.dart';
-//import 'package:mobile_app_organaizo/pages/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main(){
-  runApp(const MyApp(
-   // title: 'OrganAIzo',
-
-   // initialRoute: '/',
-
-   // routes: {
-   //   '/': (context) => const SplashScreen(),
-   //   '/navigation': (context) => const Navigation(),
-   //   '/home': (context) => const HomePage(),
-   //   '/kalender': (context) => const TableBasicsExample(),
-   // },
-
-  ));
-
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen());
-      }}
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text('error');
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Text('loading');
+      },
+    );
+  }
+}
