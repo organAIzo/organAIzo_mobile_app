2d468e3ddf0b086e1d10f6a77357df37fcf33144
Patrick Fuchs <p.fuchs@hm.edu>
2023-11-22 22:42:27 +0100

    added firebase connection

--- mobile_app_organaizo/lib/main.dart
+++ mobile_app_organaizo/lib/main.dart
@@ -1,42 +1,27 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_app_organaizo/pages/splash_screen.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(App());
+
+Future<void> firebase() async {
+ await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+}
+void main(){
+  runApp(const MyApp(
+   
+  ));
+
 }
 
-class App extends StatefulWidget {
-  const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  /// The future is part of the state of our widget. We should not call `initializeApp`
-  /// directly inside [build].
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return Text('error');
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp();
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return Text('loading');
-      },
-    );
-  }
-}
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen());
+      }}
--- mobile_app_organaizo/test/widget_test.dart
+++ mobile_app_organaizo/test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mobile_app_organaizo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
